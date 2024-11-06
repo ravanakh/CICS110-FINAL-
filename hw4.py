5891fd375a593f0137c4a9f840a7022cce7dff99
ravanakh <rakhundov@umass.edu>
2024-11-06 16:30:58 -0500

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -9,17 +9,17 @@ prompt()
 def chest():
     chest_items = ["Health Potion", "Greater Health Potion", "Mana Potion", "Greater Mana Potion", "Stamina Potion", "Greater Stamina Potion", "Elixir of Strength", "Elixir of Defense", "Rusty Sword (1★)", "Iron Sword (2★)", "Steel Sword (3★)", "Enchanted Blade (4★)", "Mythic Sword (5★)", "Full Restore"]
     drop_chances = [
-    30,  # Health Potion 
-    15,  # Greater Health Potion 
-    30,  # Mana Potion 
-    15,  # Greater Mana Potion 
+    30,  #Health Potion 
+    15,  #Greater Health Potion 
+    30,  #Mana Potion 
+    15,  #Greater Mana Potion 
     35,  #stamina
     15,  #greater stamina
     10,  #el of strenght
-    10,   #el of defense
-    25,  # Rusty Sword 
-    12,  # Iron Sword 
-    10,  # Steel Sword  
+    10,  #el of defense
+    25,  #Rusty Sword 
+    12,  #Iron Sword 
+    10,  #Steel Sword  
     5,   #enchanted blade 4 stars
     3, #mythic blade 5 star
     8,  #full restore of health
