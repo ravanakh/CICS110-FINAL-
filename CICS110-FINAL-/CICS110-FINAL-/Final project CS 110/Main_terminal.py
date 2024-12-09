bf89cde99f79f74f54edc8dbde74664bd7ea98d3
lolershark <haotiandong@umass.edu>
2024-12-09 16:01:32 -0500

    Update Main_terminal.py

--- CICS110-FINAL-/CICS110-FINAL-/Final project CS 110/Main_terminal.py
+++ CICS110-FINAL-/CICS110-FINAL-/Final project CS 110/Main_terminal.py
@@ -15,6 +15,17 @@ def main():
         elif choice == "3":
             print("Thank you for playing! Goodbye!")
             break
+        elif choice == "4":
+            game = PlayGame()
+            try:
+                game.load_game() 
+                print("Game loaded successfully!")
+                game.start_game() 
+                break
+            except FileNotFoundError:
+                print("No save file found. Please select another choice.")
+        else:
+            print("Invalid choice. Please select a valid option.")
 
 if __name__ == "__main__":
     main()
